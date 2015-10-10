5d96a6000187f5c4bf1708f5eb1c0bdc0c554233
beneon <lucidstudio@gmail.com>
2015-10-10 23:18:08 +0800

    已经做完了assignment1，现在准备根据课堂演示修改viewcontroller.swift了

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class ViewController: UIViewController
 {
+
     @IBOutlet weak var display: UILabel!
+    // implicitly unwrap
     //: UILabel! means the type of display
-    var userIsInTheMiddleOfTypingANumber:Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
+    var brain = CalculatorBrain()
     @IBAction func appendDigid(sender: UIButton)
     {
         let digit = sender.currentTitle!
-        println("digit = \(digit)")
+        print("digit = \(digit)")
         //optional, kinda like the Vector3? in c#
         if userIsInTheMiddleOfTypingANumber {
         display.text = display.text! + digit
@@ -25,5 +28,40 @@ class ViewController: UIViewController
             userIsInTheMiddleOfTypingANumber = true
         }
     }
-}
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle{
+            if let result = brain.performOperation(operation){
+                displayValue = result
+            }else{
+                displayValue = 0
+            }
+        }
+        
+    }
 
+    //this is creating an instance of a class
+    //type inference: operatorStack:Array<Double> = Array<Double> () >> operatorStack = Array<Double> ()
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue){
+            displayValue = result
+        }else{
+            displayValue = 0
+        }
+    }
+    var displayValue:Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+
+
+
+}
